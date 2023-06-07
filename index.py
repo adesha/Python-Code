d0ac378c2a50b6218a59ad52ebf5abb78424604c
adesha <adeshaholagodu@gmail.com>
2023-06-07 21:17:10 +0530

    types, list, tuple

---
+++ index.py
@@ -0,0 +1,46 @@
+# print('hello')
+# a=3
+# print(a)
+# a='world'
+# print(a)
+# print(a[0])
+# print (type(type(int)))
+# a = 5
+# print("Type of a: ", type(a))
+
+# b = 5.0
+# print("\nType of b: ", type(b))
+
+# c = 2 + 4j
+# print("\nType of c: ", type(c))
+
+# list=[]
+# print(list)
+# list=['geeks','for','geeks']
+# print(list)
+# print(list[0])
+# print(list[0][1])
+# list=[['abc','xyz'],'mno']
+# print(list)
+# print(list[0])
+# print(list[0][0])
+# print(list[0][0][1])
+
+# tuple1=()
+# print(tuple1)
+# tuple1 = ('Geeks', 'For')
+# print(tuple1)
+# print(tuple1[0])
+# #tuple1[0]='Geek'
+# print(tuple1)
+# print(tuple1[0])
+
+# list1 = [1, 2, 4, 5, 6]
+# print(list1)
+# print(tuple(list1))
+
+Tuple1 = (0, 1, 2, 3)
+Tuple2 = ('python', 'geek')
+Tuple3 = (Tuple1, Tuple2)
+print("\nTuple with nested tuples: ")
+print(Tuple3)
