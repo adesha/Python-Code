6cae70a12e5a10d3c95bbcb2142e124f9a52e4d4
adesha <adeshaholagodu@gmail.com>
2023-06-08 18:45:31 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,51 @@
+# x=1000000000000000000000000000000000000000000000000
+# print(x)
+# x=x+1
+# print(x)
+# print(100**100)
+
+a = 9
+b = 4
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a**b)
+# print(a%b)
+
+# print(a>b)
+# print(a<b)
+# print(a==b)
+# print(a!=b)
+# print(a>=b)
+# print(a<=b)
+# print(a is b)
+# print(a is not b)
+
+# print(a&b)
+# print(a|b)
+# print(~a)
+# print(a^b)
+# print(a>>b)
+# print(a<<b)
+
+# list1=[10,4,8,7]
+# if a not in list1:
+#     print("It's not there")
+# if b in list1:
+#     print("It's there")
+
+# x,y=10,20
+# min=x if x<y else y
+# print(min)
+
+print( (b, a) [a < b] )
+print({True: a, False: b} [a < b])
+print((lambda: b, lambda: a)[a < b]())
+
+print(a,"is greater") if (a>b) else print(b,"is Greater")
+
+min = a < b and a or b
+
+print(min)
