078af7a6d043b9839e8948c4201cf315d5e1355a
adesha <adeshaholagodu@gmail.com>
2023-06-10 22:49:17 +0530

    strings

---
+++ strings.py
@@ -0,0 +1,28 @@
+print('''I'm a "Geek"''')
+
+str='GeeksForGeeks'
+
+print(str[0:])
+print(str[-1:])
+print(str[::-1])
+
+str[0]='g'
+print(str)
+
+str='Hello World'
+print(str)
+
+
+s='Hello "World"'
+print(s)
+s="Hello 'World'"
+print(s)
+s="Geeks\"For\"Geeks"
+print(s)
+
+ch = "I\nLove\tGeeksforgeeks"
+print(ch)
+
+s='helloworld'
+t=s[::-1]
+print(t)
