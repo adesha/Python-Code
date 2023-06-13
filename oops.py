c27bae2bf968f3f3e278c9d046e2afdb813285cb
adesha <adeshaholagodu@gmail.com>
2023-06-13 21:49:34 +0530

    updated code

--- oops.py
+++ oops.py
@@ -1,110 +1,110 @@
-# class Person:
-#     def __init__(self,name):
-#         print('Constructor')
-#         self.name=name
+class Person:
+    def __init__(self,name):
+        print('Constructor')
+        self.name=name
 
-#     def show(self):
-#         print(self.name)
+    def show(self):
+        print(self.name)
 
-#     def __del__(self):
-#         print('destructor')
+    def __del__(self):
+        print('destructor')
 
-# p = Person('Arun')
-# p.show()
+p = Person('Arun')
+p.show()
 
-# # Inheritance
-# class Person:
-#     def __init__(self,name):
-#         self.name=name
+# Inheritance
+class Person:
+    def __init__(self,name):
+        self.name=name
     
-#     def getName(self):
-#         return self.name
+    def getName(self):
+        return self.name
     
-#     def isEmployee(self):
-#         return False
+    def isEmployee(self):
+        return False
     
-# class Employee(Person):
-#     def isEmployee(self):
-#         return True
-    
-
-# p=Person('Rahul')
-# print(p.getName(),p.isEmployee())
-
-# e=Employee('Ajith')
-# print(e.getName(),e.isEmployee())
-
-# class base:
-#     def __init__(self,name):
-#         self.name=name
-    
-#     def getName(self):
-#         return self.name
-    
-# class child(base):
-#     def __init__(self, name,age):
-#         base.__init__(self,name)
-#         self.age=age
-    
-#     def getAge(self):
-#         return self.age
-    
-# class grandChild(child):
-#     def __init__(self,name,age,address):
-#         child.__init__(self,name,age)
-#         self.address=address
-    
-#     def getAddress(self):
-#         return self.address
+class Employee(Person):
+    def isEmployee(self):
+        return True
     
 
-# gc=grandChild('abc',5,'KA')
-# print(gc.getName(),gc.getAge(),gc.getAddress())
+p=Person('Rahul')
+print(p.getName(),p.isEmployee())
 
-# class C(object):
-#     def __init__(self):
-#         self.c=21
-#         self.__d=42 #Private
+e=Employee('Ajith')
+print(e.getName(),e.isEmployee())
 
-# class D(C):
-#     def __init__(self):
-#         self.e=10
-#         C.__init__(self)
+class base:
+    def __init__(self,name):
+        self.name=name
+    
+    def getName(self):
+        return self.name
+    
+class child(base):
+    def __init__(self, name,age):
+        base.__init__(self,name)
+        self.age=age
+    
+    def getAge(self):
+        return self.age
+    
+class grandChild(child):
+    def __init__(self,name,age,address):
+        child.__init__(self,name,age)
+        self.address=address
+    
+    def getAddress(self):
+        return self.address
+    
 
-# d=D()
-# print(d.e,d.c)
+gc=grandChild('abc',5,'KA')
+print(gc.getName(),gc.getAge(),gc.getAddress())
 
-# # Polymorphism
-# def add(x,y,z=0):
-#     return x+y+z
+class C(object):
+    def __init__(self):
+        self.c=21
+        self.__d=42 #Private
 
-# print(add(2,3))
-# print(add(2,3,5))
+class D(C):
+    def __init__(self):
+        self.e=10
+        C.__init__(self)
+
+d=D()
+print(d.e,d.c)
+
+# Polymorphism
+def add(x,y,z=0):
+    return x+y+z
+
+print(add(2,3))
+print(add(2,3,5))
 
 
-# class bird:
-#     def show(self):
-#         print('bird')
-#     def flight(self):
-#         print('fly')
+class bird:
+    def show(self):
+        print('bird')
+    def flight(self):
+        print('fly')
 
-# class sparrow(bird):
-#     def flight(self):
-#         print('fly sparrow')
+class sparrow(bird):
+    def flight(self):
+        print('fly sparrow')
 
-# class peacock(bird):
-#     def flight(self):
-#         print('peacock fly')
+class peacock(bird):
+    def flight(self):
+        print('peacock fly')
 
-# b=bird()
-# b.show()
-# b.flight()
-# s=sparrow()
-# s.show()
-# s.flight()
-# p=peacock()
-# p.show()
-# p.flight()
+b=bird()
+b.show()
+b.flight()
+s=sparrow()
+s.show()
+s.flight()
+p=peacock()
+p.show()
+p.flight()
 
 # Encapsulation
 class base:
