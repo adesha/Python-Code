ebeeaf30fa64eb9bcc90705774a9c34f491e17b0
adesha <adeshaholagodu@gmail.com>
2023-06-17 21:35:06 +0530

    Arrays and lists

--- DSA.py
+++ DSA.py
@@ -1,2 +1,36 @@
 # Linear - Array,Stack,Queue,Linked list,Hash table 
 # Non-Linear - Trees, Graph,
+
+# import array as arr
+
+# a=arr.array('i',[1,2,3])
+# print(a)
+# for i in range(0,3):
+#     print(a[i],end=" ")
+
+# print('')
+
+# a.insert(1,4)
+# print(a)
+
+# a.pop()
+# print(a)
+# a.pop(1)
+# print(a)
+# a[1]=6
+# print(a)
+# print(len(a))
+list=[1,2,3]
+print(len(list))
+list.append(4)
+print(list)
+list.insert(0,6)
+print(list)
+list.extend([8,9,7])
+print(list)
+list.remove(6)
+print(list)
+list.pop()
+print(list)
+list.pop(4)
+print(list)
